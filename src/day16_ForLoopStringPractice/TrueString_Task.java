0a57422fc0ae0a38116f3599685401c98de7adc2
Muhammet Çiftçi <mciftci5507@gmail.com>
2023-01-04 15:58:30 +0300

    TrueString task is done.

---
+++ src/day16_ForLoopStringPractice/TrueString_Task.java
@@ -0,0 +1,34 @@
+package day16_ForLoopStringPractice;
+
+import java.util.Scanner;
+
+public class TrueString_Task {
+
+    public static void main(String[] args) {
+/*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter some words includes \"cat\"s and \"dog\"s");
+
+        String catAndDogs = scanner.nextLine();
+
+       String cat = catAndDogs.;
+
+        System.out.println(cat);
+        
+ */
+    }
+}
+/*
+write a program to print true
+if the string "cat" and "dog"
+appear the same number of times
+in the given sentence
+
+	        Ex:
+	            sentence = "caT dog dogG cAt"
+
+	            output:
+	                true
+
+ */
