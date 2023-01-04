05e41c83ad8a5af3c45523013848669b13d7052d
Muhammet Çiftçi <mciftci5507@gmail.com>
2023-01-04 14:40:01 +0300

    MilesToKm task is done

---
+++ src/day12_Scanner/MilesToKM.java
@@ -0,0 +1,30 @@
+package day12_Scanner;
+
+import java.util.Scanner;
+
+public class MilesToKM {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter miles:");
+
+        double miles = scanner.nextDouble();
+
+        double kM = miles * 1.609;
+
+        System.out.println(miles + " miles equal to " + kM + " kilometers.");
+    }
+}
+
+/*
+ Write a program that can convert Miles to KM
+            Ex:
+                Enter miles:
+                10.0
+
+                output:
+                10.0 miles equal to 16.09 kilometers
+
+ */
