0689892e856b2b995ff3c55e0016bb10965efb22
Muhammet Çiftçi <mciftci5507@gmail.com>
2023-01-04 15:20:05 +0300

    PositiveOrNegative task is done

---
+++ src/day16_ForLoopStringPractice/PositiveOrNegative_Task.java
@@ -0,0 +1,42 @@
+package day16_ForLoopStringPractice;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative_Task {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int positive = 0;
+        int negative = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Please enter a number");
+            int num = scanner.nextInt();
+            if (num < 0) {
+                negative++;
+            } else {
+                positive++;
+            }
+        }
+
+        System.out.println(positive + " positive and " + negative + " negative");
+
+
+    }
+}
+/*
+Write a program that asks user to enter number for 5 times,
+ and print how many positive and negative numbers user entered
+			Ex:
+				Inputs:
+					10
+					20
+					-1
+					0
+					3
+
+				Output:
+					3 positive and 1 negative
+ */
