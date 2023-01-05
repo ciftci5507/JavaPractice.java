c4f9dd2c5f8c223c96c7b18ebdf6019f4370b566
Muhammet Çiftçi <mciftci5507@gmail.com>
2023-01-05 22:48:16 +0300

    Shape class is created and Car objects are changed

---
+++ src/day40_FinalKeyword/EncapsulationReview.java
@@ -0,0 +1,17 @@
+package day40_FinalKeyword;
+
+import day34_GarbageCollection_AccessModifiers.Circle;
+
+public class EncapsulationReview {
+
+    private Circle circle;
+
+    public Circle getCircle() {
+        return circle;
+    }
+
+    public void setCircle(Circle circle) {
+        this.circle = circle;
+    }
+
+}
