1974634b286781982e57530411a9b699c1a2d4ff
Muhammet Çiftçi <mciftci5507@gmail.com>
2023-01-05 23:42:25 +0300

    Rectangle class is done.

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -1,6 +1,6 @@
 package day39_Recap.shapeTask;
 
-public class Rectangle {
+public class Rectangle extends Shape {
 
     private double length, width;
 
@@ -27,4 +27,31 @@ public class Rectangle {
         }
         this.width = width;
     }
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+
 }
